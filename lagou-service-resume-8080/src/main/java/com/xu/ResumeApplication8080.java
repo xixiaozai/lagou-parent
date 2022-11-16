6f6effe786c961f558099bd655ac963bfa864831
xixiaozai <7442743+xixiaozai@user.noreply.gitee.com>
2022-11-16 22:21:41 +0800

    增加对nacos的引入

--- lagou-service-resume-8080/src/main/java/com/xu/ResumeApplication8080.java
+++ lagou-service-resume-8080/src/main/java/com/xu/ResumeApplication8080.java
@@ -14,7 +14,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EntityScan("com.xu.pojo")
 @EnableDiscoveryClient
-@EnableEurekaClient
+//@EnableEurekaClient
 public class ResumeApplication8080 {
     public static void main(String[] args) {
         SpringApplication.run(ResumeApplication8080.class, args);
--- lagou-service-resume-8081/src/main/java/com/xu/ResumeApplication8081.java
+++ lagou-service-resume-8081/src/main/java/com/xu/ResumeApplication8081.java
@@ -14,7 +14,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EntityScan("com.xu.pojo")
 @EnableDiscoveryClient
-@EnableEurekaClient
+//@EnableEurekaClient
 public class ResumeApplication8081 {
     public static void main(String[] args) {
         SpringApplication.run(ResumeApplication8081.class, args);
